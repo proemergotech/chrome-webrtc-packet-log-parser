1037247602f0acc47d649d2c8944c73c775517be
Gabor Pongracz <gabor.pongracz@proemergotech.com>
2020-05-19 08:37:46 +0200

    Transport Layer CC
    Update to download proto from chromium source.

--- generate.sh
+++ generate.sh
@@ -4,6 +4,6 @@ set -e
 
 rm -f /tmp/proto/rtc_event_log*
 
-wget -O /tmp/proto/rtc_event_log.proto https://raw.githubusercontent.com/jitsi/webrtc/master/logging/rtc_event_log/rtc_event_log.proto
+wget -q -O - https://chromium.googlesource.com/external/webrtc/+/refs/heads/master/logging/rtc_event_log/rtc_event_log.proto?format=TEXT | base64 -d > /tmp/proto/rtc_event_log.proto
 
 cd /tmp/proto && protoc --proto_path=/tmp/proto --go_out=plugins=unmarshaler_all:. /tmp/proto/rtc_event_log.proto
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.1
-	github.com/pion/rtcp v1.2.1
+	github.com/pion/rtcp v1.2.2-0.20200509080250-ba3bfe4ae87c
 )
 
-replace github.com/pion/rtcp => /home/pongi/gohack/github.com/pion/rtcp
+replace github.com/pion/rtcp => github.com/proemergotech/rtcp v1.2.2-0.20200518131901-832aad80eb6a
